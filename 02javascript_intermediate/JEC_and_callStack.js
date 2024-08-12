674a8c5490e04ce3f6cb9c2fc49d47907edee184
Yogendra-Bisht <bishtyogendra96436372@gmail.com>
2024-08-12 05:35:02 +0000

    this is about javaScript execution context

---
+++ 02javascript_intermediate/JEC_and_callStack.js
@@ -0,0 +1,5 @@
+// Before we start call stack we need to learn how the javascript engine execute the written code in ide
+// So there is basically three phases - 1. global execution context 2. Memory allocation 3. Code execution 
+// The Global execution context will always be there in any case 
+
+// It is an theoritical concept so e have studied about this in notebook 
